7d20e58dd800401ddb9cff885fff8e90a91a7ded
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2024-08-05 01:48:45 +0300

    trigger the cubit in bottom sheet and add the bloc consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
+import 'package:note_app/simple_bloc_observer.dart';
 import 'package:note_app/views/notes_view.dart';
 import 'constants.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
    Hive.registerAdapter(NoteModelAdapter());
@@ -16,17 +20,22 @@ class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-//ممكن كده وممكن حاجه تانيه
-      //theme: ThemeData.dark(),
-      theme: ThemeData(
-        brightness:Brightness.dark,
-        fontFamily: "Poppins"
+    return  MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=>AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+      //ممكن كده وممكن حاجه تانيه
+        //theme: ThemeData.dark(),
+        theme: ThemeData(
+          brightness:Brightness.dark,
+          fontFamily: "Poppins"
+
+        ),
+        home:const NotesView()
 
       ),
-      home:const NotesView()
-
     );
   }
 }
