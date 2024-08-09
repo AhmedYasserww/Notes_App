773488639780e9ca603a3049b2388baf3a6c2e8f
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2024-08-10 02:53:46 +0300

    fix issue with keyboard

--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -14,6 +14,7 @@ class NoteViewBody extends StatelessWidget {
         shape:const CircleBorder(),
         onPressed: () {
           showModalBottomSheet(
+           isScrollControlled: true,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16)
             ),
